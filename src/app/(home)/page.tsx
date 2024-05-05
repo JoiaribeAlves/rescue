2723af77d12f4f7cab58385c669f7bcc0382d38f
Joiaribe G. Alves <joiaribegalves@protonmail.com>
2024-05-05 19:02:02 -0400

    feat: update title

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -3,13 +3,15 @@ import type { Metadata } from "next";
 import { HelpForm } from "./components/HelpForm";
 
 export const metadata: Metadata = {
-  title: "Pedir resgate - Enchentes RS",
+  title: "Solicitar resgate - Enchentes RS",
   description: "Aqui você poderá pedir por resgate",
 };
 
 export default function Home() {
   return (
-    <div className="py-8 px-4 lg:px-8">
+    <div className="py-8 px-4 lg:px-8 flex flex-col gap-6">
+      <h1 className="font-semibold text-lg">Solicitar resgate</h1>
+
       <HelpForm />
     </div>
   );
