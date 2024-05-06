eff17614695ea49a649bea840dbb4a81742eeb6e
Joiaribe G. Alves <joiaribegalves@protonmail.com>
2024-05-06 01:10:48 -0400

    feat: add Missing page

---
+++ src/app/desaparecidos/page.tsx
@@ -0,0 +1,8 @@
+export default function Missing() {
+  return (
+    <div className="py-8 px-4 lg:px-8 flex flex-col gap-6">
+      <h1 className="font-semibold text-lg">Lista de desaparecidos</h1>
+      <p>Em breve...</p>
+    </div >
+  )
+}
