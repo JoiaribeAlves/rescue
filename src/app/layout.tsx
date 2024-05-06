81a596cc354b5602819a4574175e4a5fbd81352c
Joiaribe G. Alves <joiaribegalves@protonmail.com>
2024-05-06 00:22:48 -0400

    feat: add Footer component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
 import { Header } from "@/components/Header";
+import { Footer } from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,7 @@ export default function RootLayout({
           {children}
           <Toaster />
         </main>
+        <Footer />
       </body>
     </html>
   );
