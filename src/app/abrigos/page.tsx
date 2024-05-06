61f7de4644d46ccd3174772c423343e14de7d992
Joiaribe G. Alves <joiaribegalves@protonmail.com>
2024-05-06 01:10:29 -0400

    feat: add Shelter page

---
+++ src/app/abrigos/page.tsx
@@ -0,0 +1,8 @@
+export default function Shelter() {
+  return (
+    <div className="py-8 px-4 lg:px-8 flex flex-col gap-6">
+      <h1 className="font-semibold text-lg">Lista de abrigos</h1>
+      <p>Em breve...</p>
+    </div >
+  )
+}
