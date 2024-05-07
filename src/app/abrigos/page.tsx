3664d7fba18d08602c3cc054afbe48878f0cdcd5
Joiaribe G. Alves <joiaribegalves@protonmail.com>
2024-05-07 05:55:47 -0400

    feat: add link to my whatsapp

--- src/app/abrigos/page.tsx
+++ src/app/abrigos/page.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 
 import { SheltersList } from "./components/SheltersList";
+import Link from "next/link";
 
 export const metadata: Metadata = {
 	title: "Lista de abrigos - Enchentes RS",
@@ -11,6 +12,11 @@ export default function Shelter() {
 	return (
 		<div className="py-8 px-4 lg:px-8 flex flex-col gap-6">
 			<h1 className="font-semibold text-lg">Lista de abrigos</h1>
+
+			<p>
+				Ajude-me a atualizar esta lista, conhece um abrigo?{" "}
+				<Link href="https://wa.me/556993390705" className="font-medium">Envie o endereço aqui</Link>
+			</p>
 
 			<SheltersList />
 		</div>
