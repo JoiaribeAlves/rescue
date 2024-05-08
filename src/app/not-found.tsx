3b6d010ff19047207413c79a3605e39d214184b2
Joiaribe G. Alves <joiaribegalves@protonmail.com>
2024-05-08 02:53:25 -0400

    refactor: remove Button element

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,13 +1,19 @@
-import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
 export default function NotFound() {
 	return (
-		<div className="flex flex-1 items-center justify-center ">
-			<div className="flex flex-col w-fit gap-5 items-center justify-center ">
-				<h2 className="text-3xl lg:text-4xl font-semibold text-center">Oooops, página não encontrada!</h2>
-				<Link href="/" className="">
-					<Button className="w-full">Voltar para tela inicial</Button></Link>
+		<div className="flex flex-1 items-center justify-center">
+			<div className="flex flex-col w-fit gap-5 items-center justify-center">
+				<h2 className="text-3xl lg:text-4xl font-semibold text-center">
+					Ooops, página não encontrada!
+				</h2>
+
+				<Link
+					href="/"
+					className="p-3 bg-primary text-primary-foreground hover:bg-primary/90 rounded-md"
+				>
+					Voltar para tela inicial
+				</Link>
 			</div>
 		</div>
 	);
