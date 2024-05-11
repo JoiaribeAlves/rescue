c8b644a2a83bdc780bb61415f66672b675c60a63
Joiaribe G. Alves <joiaribegalves@protonmail.com>
2024-05-11 01:57:45 -0400

    feat: update interfaces. Types are equals database

--- src/interfaces/address.ts
+++ src/interfaces/address.ts
@@ -2,9 +2,9 @@ export interface IAddress {
 	street: string;
 	number: string;
 	district: string;
-	referencePoint?: string;
-	zipCode: string;
+	referencePoint: string | null;
+	zipCode: string | null;
 	city: string;
 	state: string;
-	mapUrl: string;
+	mapUrl: string | null;
 }
