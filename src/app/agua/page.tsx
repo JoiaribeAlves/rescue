b857e787fa92c73354f6535f072e6e61c7dc6aa0
Joiaribe Alves <102931920+JoiaribeAlves@users.noreply.github.com>
2024-06-13 22:26:32 -0400

    refactor: remove ContactList component

--- src/app/agua/page.tsx
+++ src/app/agua/page.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 
 import { WaterList } from "./components/WaterList";
-import { ContactList } from "@/components/ContactList";
 
 export const metadata: Metadata = {
 	title: "Lista de locais com água potável - Enchentes RS",
@@ -12,9 +11,7 @@ export default function Shelter() {
 	return (
 		<div className="py-8 px-4 lg:px-8 flex flex-col gap-6">
 			<h1 className="font-semibold text-lg">Lista de locais com água potável</h1>
-
-			<ContactList />
-
+			
 			<WaterList />
 		</div>
 	);
